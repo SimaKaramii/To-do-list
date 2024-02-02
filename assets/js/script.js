af777ab46403ddb7e1a924627d856352f8a681d7
SimaKarami <simakarami1996@gmail.com>
2024-02-02 21:25:07 +0330

    Add script del and add to card

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 $('select').change(function (){
     var val = $(this).val();
     var card = $(this).parents('.card');
-    console.log(val)
     if(val==0){
         $("#open").append(card);
     }else if (val==1){
@@ -16,15 +15,40 @@ $('select').change(function (){
 })
 
 //open modal
-var modal = document.getElementById('modal');
-let email = document.getElementById('email');
-let text = document.getElementById('text');
-function OpenModal(){
-    modal.style.display = 'block';
+
+var state , StateVal;
+$('.OpenModal').click(function (){
+    $('.modal').show()
+    state = $(this).parents('.task').attr('id')
+    StateVal = $(this).parents('.task').attr('option-val')
+})
+function closeModal(){
+    $('#TitleModal').val("")
+    $('#CommentModal').val("");
+    $('.modal').hide()
 }
-function CloseModal(){
-    modal.style.display = 'none';
-    email.style.borderColor = '#424874'
-    text.innerHTML = " ";
-    email.value = " ";
-}
+$('.modal-close').click(function (){
+    closeModal()
+})
+$('.AddToTable').click(function (){
+    var TitleModal = $('#TitleModal').val();
+    var text = $('#CommentModal').val();
+    $('#'+state).append('<div class="card">' +
+        '                <h6 class="TitleCard">'+TitleModal+'</h6>' +
+        '                <p class="CommentCard">'+text+'</p><div class="flex">\n' +
+        '                <select class="NewAdd">\n' +
+        '                    <option value="0">Open</option>\n' +
+        '                    <option value="1">In progress</option>\n' +
+        '                    <option value="2">Resolved</option>\n' +
+        '                    <option value="3">On hold</option>>\n' +
+        '                </select>\n' +
+        '                <div><i class="fa fa-trash" aria-hidden="true"></i></div>\n' +
+        '            </div></div>')
+    console.log( )
+    $('#'+state).find('select.NewAdd').val(StateVal);
+    closeModal()
+});
+
+$('.fa-trash').click(function (){
+    $(this).parents('.card').remove()
+})
